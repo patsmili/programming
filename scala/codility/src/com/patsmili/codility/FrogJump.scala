b7205c99b1b40ad912814a5fc1f8a6c3d8c23a2f
patsmili <patsmili@yahoo.com>
2019-10-21 04:33:07 +0530

    defn added

--- scala/codility/src/com/patsmili/codility/FrogJump.scala
+++ scala/codility/src/com/patsmili/codility/FrogJump.scala
@@ -1,14 +1,14 @@
 package com.patsmili.codility
 
 '''
-Count minimal number of jumps from position X to Y
+Count minimal number of jumps from position initialLoc to targetLoc
 '''
 
 object FrogJump{
-	def solution(X: Int, Y: Int, D: Int): Int = {
-		val distance: Long = Y - X
-		val minimum = distance / D
-		if (distance % D > 0) (minimum + 1).toInt
-		else minimum.toInt
+	def solution(intialLoc: Int, targetLoc: Int, stepJump: Int): Int = {
+		val distance: Long = targetLoc - intialLoc
+		val minimumJmp = distance / stepJump
+		if (distance % stepJump > 0) (minimumJmp + 1).toInt
+		else minimumJmp.toInt
 	}
 }
